e114ef8345c9b82b553de00de2881398403869ca
Eric Tran <erictran1547@gmail.com>
2023-07-21 18:49:59 -0700

    Added data.js

---
+++ data/data.js
@@ -0,0 +1,50 @@
+let listings = [
+    {
+        name: "Doe Memorial Library",
+        numOfStars: 5,
+        wifi: true,
+        foodDrink: false,
+        quietSpace: true,
+        onCampus: true,
+        outlets: true,
+        restrooms: true,
+        public: false,
+        address: "Doe Library, Campanile Way, Berkeley, CA 94720"
+    },
+    {
+        name: "Moffit Library 4th Floor",
+        numOfStars: 0,
+        wifi: true,
+        foodDrink: true,
+        quietSpace: false,
+        onCampus: true,
+        outlets: true,
+        restrooms: true,
+        public: false,
+        address: "Doe Library, Campanile Way, Berkeley, CA 94720"
+    },
+    {
+        name: "Free Speech Movement Cafe",
+        numOfStars: 4,
+        wifi: true,
+        foodDrink: true,
+        quietSpace: false,
+        onCampus: true,
+        outlets: false,
+        restrooms: false,
+        public: true,
+        address: "3rd Floor, UC, University Dr, Berkeley, CA 94720"
+    },
+    {
+        name: "East Asian Library",
+        numOfStars: 3,
+        wifi: true,
+        foodDrink: false,
+        quietSpace: true,
+        onCampus: true,
+        outlets: true,
+        restrooms: true,
+        public: false,
+        address: "C. V. Starr East Asian Library, Berkeley, CA 94720"
+    }
+]
